98c3423aae0ced45731f9c504fd4dc3343389476
Apple <apple@zt.local>
2020-10-11 17:34:44 +0800

    更新数据源

--- DbSource/DbSource.cs
+++ DbSource/DbSource.cs
@@ -6,11 +6,11 @@ namespace Platform.Source
 {
     //
     [DisplayName("抽象数据库")]
-    public abstract class DbSource : AbstractSource
+    public abstract class DbSource : Source
     {
         //
-        [DisplayName("加载表")]
-        public override int Load(DataTable table, string sql, params string[] none)
+        [DisplayName("通过sql加载表")]
+        public override DataTable Load(string sql, params string[] ignore)
         {
             if(_factory == null || _connectionString == null)
                 Init();
@@ -21,19 +21,23 @@ namespace Platform.Source
                 DbCommand command = _factory.CreateCommand();
                 command.Connection = connection;
                 command.CommandText = sql;
+                DataTable table = new DataTable();
                 table.ExtendedProperties.Add("sql", sql);
                 DbDataAdapter adapter = _factory.CreateDataAdapter();
                 adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
                 adapter.SelectCommand = command;
                 int count = adapter.Fill(table);
                 connection.Close();
-                return count;
+                return table;
             }
         }
         //
         [DisplayName("保存表")]
-        public override int Save(DataTable table, string sql)
+        public override int Save(DataTable table)
         {
+            string sql = table.ExtendedProperties.Contains("sql") ? 
+                table.ExtendedProperties["sql"].ToString() :
+                "select * from " + table.TableName + " where 1=2 ";
             if(_factory == null || _connectionString == null)
                 Init();
             using(DbConnection connection = _factory.CreateConnection())
